a037c9db1a585ed3ea9727d4b9e5b997a27c6a82
CN105 <cn105@CN0105.local>
2019-09-24 16:59:12 +0100

    Updates

---
+++ precache-manifest.ccd3d0a47e22f46dd48ad5b6ed9398e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bfbf1c89996a5b0a9eb4c38b1097e6",
+    "url": "/addressBook-react/index.html"
+  },
+  {
+    "revision": "26adc25e5f4f27b84b80",
+    "url": "/addressBook-react/static/css/main.87260d41.chunk.css"
+  },
+  {
+    "revision": "9cdb85526cc9edf7a061",
+    "url": "/addressBook-react/static/js/2.c2c0d217.chunk.js"
+  },
+  {
+    "revision": "26adc25e5f4f27b84b80",
+    "url": "/addressBook-react/static/js/main.8964efc7.chunk.js"
+  },
+  {
+    "revision": "5087e6539479ea2c643d",
+    "url": "/addressBook-react/static/js/runtime~main.c6f870e5.js"
+  }
+]);
